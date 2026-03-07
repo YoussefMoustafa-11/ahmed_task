1e09dda7cf5521157a90c21ec0f7bf740f8ce601
YoussefMoustafa-11 <ymoustafa406@gmail.com>
2026-03-07 16:42:30 +0200

    Add Explore and Notifications views with routing and bottom navigation integration

--- lib/Core/widgets/custom_bottom_navigation_bar.dart
+++ lib/Core/widgets/custom_bottom_navigation_bar.dart
@@ -1,25 +1,43 @@
-import 'package:flutter/material.dart';
+import 'package:ahmed_task/Core/routing/app_routes.dart';
 import 'package:ahmed_task/Core/themes/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomBottomNavigationBar extends StatefulWidget {
+class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
 
   @override
-  State<CustomBottomNavigationBar> createState() =>
-      _CustomBottomNavigationBarState();
-}
-
-class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
-  int _currentIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
+    final currentRoute = GoRouterState.of(context).matchedLocation;
+
+    int currentIndex = 0;
+    if (currentRoute == AppRoutes.homeView)
+      currentIndex = 0;
+    else if (currentRoute == AppRoutes.exploreView)
+      currentIndex = 1;
+    else if (currentRoute == AppRoutes.notificationsView)
+      currentIndex = 2;
+    else if (currentRoute == AppRoutes.profileView)
+      currentIndex = 3;
+
     return BottomNavigationBar(
-      currentIndex: _currentIndex,
+      backgroundColor: Colors.white,
+      currentIndex: currentIndex,
       onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-        });
+        switch (index) {
+          case 0:
+            context.go(AppRoutes.homeView);
+            break;
+          case 1:
+            context.go(AppRoutes.exploreView);
+            break;
+          case 2:
+            context.go(AppRoutes.notificationsView);
+            break;
+          case 3:
+            context.go(AppRoutes.profileView);
+            break;
+        }
       },
       type: BottomNavigationBarType.fixed,
       selectedItemColor: AppColors.primary,
@@ -27,16 +45,23 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
       selectedLabelStyle: const TextStyle(fontSize: 12),
       unselectedLabelStyle: const TextStyle(fontSize: 12),
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.home),
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          activeIcon: Icon(Icons.search),
           icon: Icon(Icons.explore_outlined),
           label: 'Explore',
         ),
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.notifications),
           icon: Icon(Icons.notifications_outlined),
-          label: 'Alerts',
+          label: 'Notifications',
         ),
         BottomNavigationBarItem(
+          activeIcon: Icon(Icons.person),
           icon: Icon(Icons.person_outline),
           label: 'Profile',
         ),
